0e472cdedc038483111b3d434ecd97c88f222056
Gennaro Raiola <gennaro.raiola@gmail.com>
2021-11-01 19:01:49 +0100

    Add the possibility to load the initial value from the ros param server

--- include/rt_gui/rt_gui_client.h
+++ include/rt_gui/rt_gui_client.h
@@ -36,8 +36,10 @@ public:
     return istance;
   }
 
-  void addInt(const std::string& group_name, const std::string& data_name, const int& min, const int& max, int* data_ptr, bool sync = true)
+  void addInt(const std::string& group_name, const std::string& data_name, const int& min, const int& max, int* data_ptr, bool sync = true, bool load_init_from_server = false)
   {
+    if(load_init_from_server)
+      loadFromServer(group_name,data_name,*data_ptr);
     int_h_->add(group_name,data_name,min,max,data_ptr,sync);
   }
 
@@ -46,20 +48,36 @@ public:
     int_h_->add(group_name,data_name,min,max,data,fun,sync);
   }
 
-  void addInt(const std::string& group_name, const std::string& data_name, const int& min, const int& max, std::vector<int>* data_ptr, bool sync = true)
+  void addInt(const std::string& group_name, const std::string& data_name, const int& min, const int& max, std::function<void(int)> fun, bool sync = true)
   {
+    int init_value;
+    if(loadFromServer(group_name,data_name,init_value))
+      int_h_->add(group_name,data_name,min,max,init_value,fun,sync);
+  }
+
+  void addInt(const std::string& group_name, const std::string& data_name, const int& min, const int& max, std::vector<int>* data_ptr, bool sync = true, bool load_init_from_server = false)
+  {
+    if(load_init_from_server)
+      loadFromServer(group_name,data_name,*data_ptr);
     for(unsigned int i=0;i<data_ptr->size();i++)
       int_h_->add(group_name,data_name+"["+std::to_string(i)+"]",min,max,&data_ptr->at(i),sync);
   }
 
-  void addInt(const std::string& group_name, const std::string& data_name, const int& min, const int& max, Eigen::VectorXi* data_ptr, bool sync = true)
+  void addInt(const std::string& group_name, const std::string& data_name, const int& min, const int& max, Eigen::VectorXi* data_ptr, bool sync = true, bool load_init_from_server = false)
   {
+    std::vector<int> std_v;
+    if(load_init_from_server)
+      loadFromServer(group_name,data_name,std_v);
+    for(unsigned int i=0;i<std_v.size();i++) // Copy
+      data_ptr->operator[](i) = std_v[i];
     for(unsigned int i=0;i<data_ptr->size();i++)
       int_h_->add(group_name,data_name+"["+std::to_string(i)+"]",min,max,&data_ptr->operator[](i),sync);
   }
 
-  void addDouble(const std::string& group_name, const std::string& data_name, const double& min, const double& max, double* data_ptr, bool sync = true)
+  void addDouble(const std::string& group_name, const std::string& data_name, const double& min, const double& max, double* data_ptr, bool sync = true, bool load_init_from_server = false)
   {
+    if(load_init_from_server)
+      loadFromServer(group_name,data_name,*data_ptr);
     double_h_->add(group_name,data_name,min,max,data_ptr,sync);
   }
 
@@ -68,21 +86,37 @@ public:
     double_h_->add(group_name,data_name,min,max,data,fun,sync);
   }
 
-  void addDouble(const std::string& group_name, const std::string& data_name, const double& min, const double& max, std::vector<double>* data_ptr, bool sync = true)
+  void addDouble(const std::string& group_name, const std::string& data_name, const double& min, const double& max, std::function<void(double)> fun, bool sync = true)
   {
+    int init_value;
+    if(loadFromServer(group_name,data_name,init_value))
+      double_h_->add(group_name,data_name,min,max,init_value,fun,sync);
+  }
+
+  void addDouble(const std::string& group_name, const std::string& data_name, const double& min, const double& max, std::vector<double>* data_ptr, bool sync = true, bool load_init_from_server = false)
+  {
+    if(load_init_from_server)
+      loadFromServer(group_name,data_name,*data_ptr);
     for(unsigned int i=0;i<data_ptr->size();i++)
       double_h_->add(group_name,data_name+"["+std::to_string(i)+"]",min,max,&data_ptr->at(i),sync);
   }
 
   template<typename Derived>
-  void addDouble(const std::string& group_name, const std::string& data_name, const double& min, const double& max, Eigen::MatrixBase<Derived>* data_ptr, bool sync = true)
+  void addDouble(const std::string& group_name, const std::string& data_name, const double& min, const double& max, Eigen::MatrixBase<Derived>* data_ptr, bool sync = true, bool load_init_from_server = false)
   {
+    std::vector<double> std_v;
+    if(load_init_from_server)
+      loadFromServer(group_name,data_name,std_v);
+    for(unsigned int i=0;i<std_v.size();i++) // Copy
+      data_ptr->operator[](i) = std_v[i];
     for(unsigned int i=0;i<data_ptr->size();i++)
       double_h_->add(group_name,data_name+"["+std::to_string(i)+"]",min,max,&data_ptr->operator[](i),sync);
   }
 
-  void addBool(const std::string& group_name, const std::string& data_name, bool* data_ptr, bool sync = true)
+  void addBool(const std::string& group_name, const std::string& data_name, bool* data_ptr, bool sync = true, bool load_init_from_server = false)
   {
+    if(load_init_from_server)
+      loadFromServer(group_name,data_name,*data_ptr);
     bool_h_->add(group_name,data_name,data_ptr,sync);
   }
 
@@ -91,8 +125,10 @@ public:
     trigger_h_->add(group_name,data_name,fun);
   }
 
-  void addList(const std::string& group_name, const std::string& data_name, const std::vector<std::string>& list, std::string* data_ptr, bool sync = true)
+  void addList(const std::string& group_name, const std::string& data_name, const std::vector<std::string>& list, std::string* data_ptr, bool sync = true, bool load_init_from_server = false)
   {
+    if(load_init_from_server)
+      loadFromServer(group_name,data_name,*data_ptr);
     list_h_->add(group_name,data_name,list,data_ptr,sync);
   }
 
@@ -129,6 +165,25 @@ private:
 
   ~RtGuiClient()
   {
+  }
+
+  template<class data_t>
+  bool loadFromServer(const std::string& group_name, const std::string& data_name, data_t& value)
+  {
+    bool res = true;
+    data_t init_value;
+    std::string ns = "/"+group_name+"/"+data_name;
+    if (!ros::param::get(ns, init_value))
+    {
+        ROS_WARN("No initial value given in namespace %s",ns.c_str());
+        res = false;
+    }
+    else
+    {
+        value = init_value;
+        res = true;
+    }
+    return res;
   }
 
   RtGuiClient(const RtGuiClient&)= delete;
