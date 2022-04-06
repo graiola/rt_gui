5d34c54768f390361b0e93cc60c5199a1c71b09f
Gennaro Raiola <gennaro.raiola@gmail.com>
2022-04-06 18:44:25 +0200

    Make .def static

--- bindings/rt_gui/py_client.cpp
+++ bindings/rt_gui/py_client.cpp
@@ -11,36 +11,35 @@ PYBIND11_MODULE(py_client, m) {
     m.doc() = "pybind11 rt_gui_client plugin";
 
     py::class_<rt_gui::RtGuiClient>(m,"RtGuiClient")
-        .def("istance", &rt_gui::RtGuiClient::getIstance, py::return_value_policy::reference)
-        .def("init", [](const std::string& ros_namespace)
+        .def_static("init", [](const std::string& ros_namespace = RT_GUI_NAMESPACE)
         {
           rt_gui::RtGuiClient::getIstance().init(ros_namespace);
         })
-        .def("addInt", [](const std::string& group_name, const std::string& data_name, const int& min, const int& max, std::function<void(int)> fun, bool sync = true)
+        .def_static("addInt", [](const std::string& group_name, const std::string& data_name, const int& min, const int& max, std::function<void(int)> fun, bool sync = true)
         {
           rt_gui::RtGuiClient::getIstance().addInt(group_name,data_name,min,max,fun,sync);
         })
-        .def("addDouble", [](const std::string& group_name, const std::string& data_name, const int& min, const int& max, std::function<void(double)> fun, bool sync = true)
+        .def_static("addDouble", [](const std::string& group_name, const std::string& data_name, const int& min, const int& max, std::function<void(double)> fun, bool sync = true)
         {
           rt_gui::RtGuiClient::getIstance().addDouble(group_name,data_name,min,max,fun,sync);
         })
-        .def("addBool", [](const std::string& group_name, const std::string& data_name, std::function<void(bool)> fun, bool sync = true)
+        .def_static("addBool", [](const std::string& group_name, const std::string& data_name, std::function<void(bool)> fun, bool sync = true)
         {
           rt_gui::RtGuiClient::getIstance().addBool(group_name,data_name,fun,sync);
         })
-        .def("addTrigger", [](const std::string& group_name, const std::string& data_name, std::function<void()> fun)
+        .def_static("addTrigger", [](const std::string& group_name, const std::string& data_name, std::function<void()> fun)
         {
           rt_gui::RtGuiClient::getIstance().addTrigger(group_name,data_name,fun);
         })
-        .def("addList", [](const std::string& group_name, const std::string& data_name, const std::vector<std::string>& list, std::function<void(std::string)> fun, bool sync = true)
+        .def_static("addList", [](const std::string& group_name, const std::string& data_name, const std::vector<std::string>& list, std::function<void(std::string)> fun, bool sync = true)
         {
           rt_gui::RtGuiClient::getIstance().addList(group_name,data_name,list,fun,sync);
         })
-        .def("remove", [](const std::string& group_name, const std::string& data_name)
+        .def_static("remove", [](const std::string& group_name, const std::string& data_name)
         {
           rt_gui::RtGuiClient::getIstance().remove(group_name,data_name);
         })
-        .def("sync", []()
+        .def_static("sync", []()
         {
           rt_gui::RtGuiClient::getIstance().sync();
         });
--- bindings/rt_gui/py_server.cpp
+++ bindings/rt_gui/py_server.cpp
@@ -18,10 +18,8 @@ PYBIND11_MODULE(py_server, m) {
              py::arg("parent") = py::none());
 
     py::class_<rt_gui::RtGuiServer>(m,"RtGuiServer")
-        .def("istance", &rt_gui::RtGuiServer::getIstance, py::return_value_policy::reference)
-        //.def("run", &rt_gui::RtGuiServer::run);
-        .def("run", [](const std::string& ros_namespace = RT_GUI_NAMESPACE) {
-        int argc = 1;
+        .def_static("run", [](const std::string& ros_namespace = RT_GUI_NAMESPACE) {
+        int argc = 2;
         char* argv[] = {strdup("py_server"), strdup(ros_namespace.c_str())};
         return rt_gui::RtGuiServer::getIstance().run(argc, argv);
     });
